c83f3bea521c0a6162b9f9319ef762b0658b652c
SeoHoJun <ghwns6659@naver.com>
2020-09-13 00:17:40 +0900

    field & column mapping

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,81 +19,12 @@ public class JpaMain {
         tx.begin(); // 데이터베이스 트랜잭션 시작
         try{ // 오류가 발생했을 때를 대비하기 위해 try - catch 문을 사용한다.
 
-            // 데이터 삽입
-            /*Member member = new Member();
+            Member member = new Member();
             member.setId(2L);
-            member.setName("HelloB");*/
+            member.setUsername("B");
+            member.setRoleType(RoleType.ADMIN);
 
-            //데이터 조회
-            /*Member findmember = em.find(Member.class, 1L);
-            System.out.println("findMember.id = " + findmember.getId());
-            System.out.println("findMember.name = " + findmember.getName());*/
-
-            // JPQL 을 통한 조건 탐색
-            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-            // JPQL 는 절대 테이블을 대상으로 쿼리를 짜지 않는다, 위의 코드의 경우 Member 객체에 대하여 쿼리를 작성한 것.
-            // 테이블을 대상으로 쿼리가 작성될 경우 객체 지향의 사상이 깨어진다.
-            for (Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }*/
-
-            //데이터 삭제
-            //em.remove(findmember);
-
-            //데이터 수정
-            /*Member findmember = em.find(Member.class, 1L);
-            // findmember.setId(3L); // 아이디 값은 변경되지 않는다. 왜?
-            findmember.setName("HelloJPA");*/
-
-            //em.persist(findmember); // 데이터 수정의 경우 persist 를 하지 않아도 된다.
-            //JPA 를 통해서 entity 를 가져오면 JPA가 관리를 해준다.
-            //JPA 가 데이터가 변경되었는지 그렇지 않은지 트랜잭션이 커밋되는 시점에 체크를 한다.
-            //변경점이 있을 경우 update query 를 만들어서 날려준다. 이후 트랜잭션이 커밋 된다.
-
-            //비영속
-            /*Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");*/
-
-            //em.persist(member); // 영속
-            //em.detach(member); // 준영속
-
-            //Member findMemeber1 = em.find(Member.class, 101L); // SELECT query 실행
-            //Member findMemeber2 = em.find(Member.class, 101L); // 같은 데이터를 찾는 경우이기 때문에 1차캐시 에서 데이터를 가져오게 된다.(SELECT query 실행되지 않음)
-            //System.out.println("findMemeber.id = " + findMemeber1.getId());
-            //System.out.println("findMemeber.name = " + findMemeber2.getName());
-            //System.out.println("result = " + (findMemeber1 == findMemeber2));
-
-            /* 영속성 컨텍스트 : 버퍼링을 통한 쓰기 지연의 예시
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            em.persist(member1);
-            em.persist(member2); // persist 하는 순간 데이터베이스에 저장되는 것이 아니라 영속성 컨텍스트에 차곡차곡 entity 와 쿼리가 쌓이게 된다.
-
-            System.out.println("=============================");*/
-
-            // 엔티티 수정(변경 감지)
-            //Member member = em.find(Member.class, 150L);
-            //member.setName("ZZZZ");
-            // em.persist(member) -> 데이터 수정시에 persist 를 활용할 필요 없다.
-            // persist() 메소드를 날리지 않아도 1차캐시에 있는 스냅샷을 통한 비교 결과 변경점이 있을 경우 update query 가 데이터베이스로 전달되게 된다.
-
-            /* flush() 직접 호출
-            Member member = new Member(200L, "member200");
             em.persist(member);
-            em.flush(); // 강제 호출*/
-
-            // 준영속 상태로 만들기
-            //Member member = em.find(Member.class, 150L); // 영속성 컨텍스트에 값을 올림
-            //member.setName("AAAA"); // Dirty Checking 을 통한 변경
-
-            //em.detach(member); // 영속성 컨텍스트에서 빼버림으로서 준영속 상태로 전환
-            // JPA 가 더 이상 해당 객체를 관리하지 않음
-            // 커밋을 해도 update query 가 나오지 않는다.(즉, 데이터의 변경이 일어나지 않는다.)
 
             tx.commit(); // 커밋하는 시점에 진짜 데이터베이스에 쿼리가 전달된다.
         } catch (Exception e){
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity // JPA 가 관리하는 클래스임을 알리는 어노테이션
@@ -14,7 +16,7 @@ public class Member {
 
     private Integer age;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated
     private RoleType roleType;
 
     @Temporal(TemporalType.TIMESTAMP)
@@ -22,6 +24,9 @@ public class Member {
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
 
     @Lob
     private String description;
@@ -33,5 +38,67 @@ public class Member {
         //그렇기 때문에 기본 생성자가 하나 있어야 한다
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
 }
