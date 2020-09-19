e181b8ff0d83afa9e3ad1416d582de76f01cfdc6
SeoHoJun <ghwns6659@naver.com>
2020-09-19 21:51:28 +0900

    Object & Database Mapping

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -17,10 +17,13 @@ public class Member {
     @Column(name = "name", nullable = false) // 데이터베이스 column 명을 name 으로 한다.
     private String username; // 객체는 username 을 쓰고 싶은데 DB 는 name 을 써야 하는 경우 Column 어노테이션의 name 속성을 활용한다.
 
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
     public Member() { // JPA 는 기본적으로 내부적으로 reflection 같은것들을 쓰기 때문에 동적으로 객체를 생성해내야 한다.
         //그렇기 때문에 기본 생성자가 하나 있어야 한다
     }
+
 
     public Long getId() {
         return id;
