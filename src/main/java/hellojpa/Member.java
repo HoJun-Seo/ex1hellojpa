e6ac46f6fbeb331359e6efc6f047fb3c10e82b33
SeoHoJun <ghwns6659@naver.com>
2020-09-24 00:22:41 +0900

    Relationship

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,54 +1,21 @@
 package hellojpa;
 
-import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-@Entity // JPA 가 관리하는 클래스임을 알리는 어노테이션
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ",
-        initialValue = 1, allocationSize = 50
-)
+@Entity
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    //@Column(unique = true, length = 10)
-    @Column(name = "name", nullable = false) // 데이터베이스 column 명을 name 으로 한다.
-    private String username; // 객체는 username 을 쓰고 싶은데 DB 는 name 을 써야 하는 경우 Column 어노테이션의 name 속성을 활용한다.
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    @Column(name = "USERNAME")
+    private String username;
 
-    @Lob
-    private String description;
-
-    @Transient
-    private Integer temp;
-
-    public Member() { // JPA 는 기본적으로 내부적으로 reflection 같은것들을 쓰기 때문에 동적으로 객체를 생성해내야 한다.
-        //그렇기 때문에 기본 생성자가 하나 있어야 한다
-    }
-
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    @Column(name = "TEAM_ID")
+    private Long teamId; // 객체 지향 스럽게 코딩하려면 Member 와 Team 클래스간에 참조로 가져가야 하나
+    // DB 테이블에 맞춰 모델링을 하고 있기 때문에 해당 필드를 직접 작성해주었다.(Team 클래스의 id 필드)
 }
