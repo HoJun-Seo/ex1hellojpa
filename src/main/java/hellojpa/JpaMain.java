ec6cde7550762814af34ddd08146488577298b8a
SeoHoJun <ghwns6659@naver.com>
2020-09-30 22:21:31 +0900

    Relationship Mapping

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,79 +19,16 @@ public class JpaMain {
         tx.begin(); // 데이터베이스 트랜잭션 시작
         try{ // 오류가 발생했을 때를 대비하기 위해 try - catch 문을 사용한다.
 
-            /* 단방향 및 양방향 연관관계
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team); // 영속성 컨텍스트에 올라갈때 항상 기본 키가 같이 매핑된다.
-
             Member member = new Member();
             member.setUsername("member1");
-            //member.setTeamId(team.getId()); // 객체지향 스러우려면 setTeam 이 되어야 하지 않을까?
-            //member.setTeam(team); // 이렇게 해두면 JPA 가 알아서 team 에서 기본 키를 꺼낸 후 insert 할 때 외래키 값으로 사용한다.
-            em.persist(member);
 
-            // Entity 를 1차캐시가 아닌 DB 에서 가져오게끔 하기 위해 영속성 컨텍스트를 비워준다.
-            em.flush();
-            em.clear();
-
-            //Member findMember = em.find(Member.class, member.getId());
-
-            //Long findTeamId = findMember.getTeamId(); // 테이블 중심으로 설계하는 바람에 이번엔 팀 아이디를 찾으면서 또다시 기본키를 찾는 과정을 반복해야 한다.
-            //Team findTeam = em.find(Team.class, findTeamId); // 기본 키 값을 한 번 찾는 걸로 팀을 알아낼 수는 없을까?
-
-            //Team findTeam = findMember.getTeam(); // 팀 데이터를 바로 꺼낼 수 있다.
-            //System.out.println("findTeam = " + findTeam.getName());
-
-            // 팀을 바꾸고 싶다면?
-            //Team newTeam = em.find(Team.class, 100L); // 키값이 100 번에 해당하는 팀이 있다고 가정한다.
-            //findMember.setTeam(newTeam); // 찾아온 멤버 데이터의 팀을 변경해준다.(외래키가 업데이트 된다.)
-
-            // 양방향 연관관계 매핑
-            List<Member> members = findMember.getTeam().getMembers();
-            for (Member m : members){
-                System.out.println("m = " + m.getUsername());
-            } // 왜 flush, clear 를 거쳐야 해당 출력문이 제대로 출력되는지는 다음강의 때 나오는 내용을 참고할 것
-            // 참조 질문글 내용 : flush, clear 를 안 하면 members 컬렉션(List) 이 비어있는 상태로 영속성 컨텍스트에 반영된다?
-             */
-
-
-
-
-
-            // 양방향 연관관계의 주의 점 1. 연관관계의 주인의 값을 입력하지 않은 경우
-            /*Member member = new Member();
-            member.setUsername("member1");
             em.persist(member);
 
             Team team = new Team();
-            team.setName("TeamA");
-            team.getMembers().add(member); // 연관관계의 주인쪽 객체에서 값을 넣지 않고 주인이 아닌곳에서 값을 넣고 있다.
-            // 그 결과 insert 가 제대로 반영되지 않아 멤버 데이터의 팀 값이 null 로 표시된다.
-            em.persist(team);*/
+            team.setName("teamA");
+            team.getMembers().add(member);
 
-            // 올바른 예시
-            Team team = new Team();
-            team.setName("TeamA");
             em.persist(team);
-
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setTeam(team);
-            em.persist(member);
-
-            team.getMembers().add(member); // getMembers 를 통해 1차 캐시에서 데이터를 찾아온 후 값을 삽입해준다.
-            // 여기서 이미 영속성 컨텍스트에 들어가 있는 entity 에 대해 수정의 경우 굳이 find 를 통해 찾아오거나 영속성 컨텍스트에서 빼낼 필요가 없었던 이유가 있었는데 기억이 나지 않는다.
-            // 나중에 블로그에 글을 정리할 때 복습하며 참고하자.
-
-            //em.flush();
-            //em.clear();
-
-            Team findTeam = em.find(Team.class, team.getId());
-            List<Member> members = findTeam.getMembers();
-
-            for (Member m : members){
-                System.out.println("m = " + m.getUsername());
-            }
 
             tx.commit(); // 커밋하는 시점에 진짜 데이터베이스에 쿼리가 전달된다.
         } catch (Exception e){
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -12,15 +12,20 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team; // 일대다 관계에서 양방향 매핑을 하고 싶은 경우
+    // 양쪽 도메인 모두 연관관계의 주인이 되는 경우를 막기 위해 insertable, updateable 속성을 false 로 지정하여 해당 객체를 읽기 전용으로 선언한다.
+
     //@Column(name = "TEAM_ID")
     //private Long teamId; // 객체 지향 스럽게 코딩하려면 Member 와 Team 클래스간에 참조로 가져가야 하나
     // DB 테이블에 맞춰 모델링을 하고 있기 때문에 해당 필드를 직접 작성해주었다.(Team 클래스의 id 필드)
 
-    @ManyToOne
+    /*@ManyToOne
     @JoinColumn(name = "TEAM_ID") // team 참조값과 데이터베이스의 외래키(TEAM_ID) 가 매핑되어야 한다.(이러면 매핑 끝남)
-    private Team team;
+    private Team team;*/
 
-    public Team getTeam() {
+    /*public Team getTeam() {
         return team;
     }
 
@@ -28,7 +33,7 @@ public class Member {
         this.team = team;
         //team.getMembers().add(this);
         // 연관관계 관련 메소드에 이와 같이 역방향 삽입 코드를 작성해 줄 수 있다.
-    }
+    }*/
 
     public Long getId() {
         return id;
