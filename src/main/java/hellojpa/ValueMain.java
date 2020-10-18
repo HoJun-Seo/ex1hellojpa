02d437d55000aee605557f41eb928004a1afab89
SeoHoJun <ghwns6659@naver.com>
2020-10-18 21:10:19 +0900

    Value & Embedded Type

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -15,5 +15,11 @@ public class ValueMain {
 
 		System.out.println("a = " + a);
 		System.out.println("b = " + b);
+
+		Address address1 = new Address("city", "street", "10000");
+		Address address2 = new Address("city", "street", "10000");
+
+		System.out.println("address1 == address2 : " + (address1 == address2));
+		System.out.println("address1 equals address2 : " + (address1.equals(address2)));
 	}
 }
